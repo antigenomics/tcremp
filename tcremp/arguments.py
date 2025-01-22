3d2acf896a7224f722ba2d5a63f291bd8b3691cc
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2025-01-23 00:32:56 +0300

    cli update

--- tcremp/arguments.py
+++ tcremp/arguments.py
@@ -7,16 +7,17 @@ def get_arguments():
                         help='Path to input file containing a clonotype (clone) table')  # ++
 
     parser.add_argument('-o', '--output', type=str, required=True,
-                        help='Path to the output folder')  # +
+                        help='Path to the output folder')  # ++
 
     parser.add_argument('-e', '--prefix', type=str,
-                        help='Output prefix. Defaults to input clonotype table filename')  # +
+                        help='Output prefix. Defaults to input clonotype table filename')  # ++
 
     parser.add_argument('-x', '--index-col', type=str,
                         help='(optional) Name of a column in the input table containing user-specified IDs that will '
-                             'be transfered to output tables')  # todo missing in readme
+                             'be transfered to output tables')  # todo missing in readme ++
 
     parser.add_argument('-c', '--chain', type=str, required=True,
+                        choices=['TRA', 'TRB', 'TRA_TRB'],
                         help='"TRA" or "TRB" for single-chain input data (clonotypes), for paired-chain input ('
                              'clones) use "TRA_TRB". Used in default prototype set selection')  # ++
 
@@ -24,38 +25,17 @@ def get_arguments():
                         help='Path to user-specified prototypes file. If not set, will use pre-built prototype tables, '
                              '"$tcremp_path/data/data_prebuilt"')  # ++
 
-    # parser.add_argument('-p_a', '--prototypes-path-alpha', type=str,
-    #                     help='Path to user-specified TCR alpha prototypes file. Can be omitted')
-    #
-    # parser.add_argument('-p_b', '--prototypes-path-beta', type=str,
-    #                     help='Path to user-specified TCR beta prototypes file. Can be omitted')
-
-    # parser.add_argument('-p_cdr3aa_col', '--prototypes-cdr3aa-column', type=str, default='cdr3aa',
-    #                     help='Name of cdr3 column for custom file with prototypes. "cdr3aa" will be used if not '
-    #                          'specified.')
-    #
-    # parser.add_argument('-p_cdr3nt_col', '--prototypes-cdr3nt-column', type=str,
-    #                     help='Name of cdr3nt column for custom file with prototypes. None will be used if not '
-    #                          'specified.')
-
-    # parser.add_argument('-p_v_col', '--prototypes-v-column', type=str, default='v',
-    #                     help='Name of v column for custom file with prototypes. "v" will be used if not '
-    #                          'specified.')
-    #
-    # parser.add_argument('-p_j_col', '--prototypes-j-column', type=str, default='j',
-    #                     help='Name of j column for custom file with prototypes. "j" will be used if not '
-    #                          'specified.')
-
     parser.add_argument('-n', '--n-prototypes', type=int,
                         help='Number of prototypes to select for clonotype "triangulation" during embedding. The '
                              'total number of co-ordinates will be (number of chains) * (3 for V, J and CDR3 '
-                             'distances) * (n). Will use all available prototypes if not set')  # +
+                             'distances) * (n). Will use all available prototypes if not set')  # ++
 
     parser.add_argument('-nc', '--n-clonotypes', type=int,
                         help='Number of clonotypes to process in the pipeline. Will use all available clonotypes'
-                             ' if not set')  # +
+                             ' if not set')  # ++
 
     parser.add_argument('-s', '--species', type=str, default='HomoSapiens',
+                        choices=['HomoSapiens', 'MusMusculus', 'MacacaMulatta'],
                         help='Prototype set species specification. Currently only "HomoSapiens" is supported')  # ++
 
     parser.add_argument('-u', '--unique-clonotypes',
@@ -65,7 +45,7 @@ def get_arguments():
                         help='Random seed for prototype sampling and other rng-based procedures')  # +
 
     parser.add_argument('-a', '--cluster-algo', type=str, default='DBSCAN',
-                        help='Embedding clustering algorithm: "DBSCAN", "K-means" or "None" to skip the step')
+                        help='Embedding clustering algorithm: "DBSCAN", "K-means" or "None" to skip the step') # -
 
     parser.add_argument('-np', '--nproc', type=int, default=1,
                         help='Number of processes to perform calculation with')  # ++
@@ -76,8 +56,8 @@ def get_arguments():
                              'calculated by comparing user-provided labels with inferred cluster labels')  # missing in readme
 
     parser.add_argument('-llen', '--lower-len-cdr3', type=int, default=5,
-                        help='(optional) filter out cdr3 with len <llen')  # +
+                        help='(optional) filter out cdr3 with len <llen')  # ++
 
     parser.add_argument('-hlen', '--higher-len-cdr3', type=int, default=30,
-                        help='(optional) filter out cdr3 with len >=hlen')  # +
+                        help='(optional) filter out cdr3 with len >=hlen')  # ++
     return parser.parse_args()
--- tcremp/new_tcremp_run.py
+++ tcremp/new_tcremp_run.py
@@ -1,10 +1,13 @@
 import time
 import pandas as pd
 
+from tcremp import get_resource_path
 from tcremp.arguments import get_arguments
 import sys
 import logging
 from pathlib import Path
+
+# sys.path.append('../../mirpy')
 
 from mir.common.segments import SegmentLibrary
 from mir.common.repertoire import Repertoire
@@ -43,24 +46,51 @@ def configure_io(args):
     output_path = Path(args.output)
     output_path.mkdir(parents=True, exist_ok=True)
     input_path = Path(args.input)
-    proto_path = Path(args.prototypes_path)
+    proto_path = Path(args.prototypes_path if args.prototypes_path else get_resource_path('tcremp_prototypes_olga.tsv'))
     output_prefix = args.prefix
     if not output_prefix:
         output_prefix = Path(input_path).stem
     return str(input_path.resolve()), output_path, output_prefix, str(proto_path.resolve())
 
 
-def load_mirpy_objects(segment_library, data_path, proto_path, single_chain=True):
-    parser = AIRRParser if single_chain else DoubleChainAIRRParser
+def validate_cdr3_len(repertoire: Repertoire, llen: int, hlen: int, single_chain):
+    if llen is None:
+        llen = -1
+    if hlen is None:
+        hlen = 35
+
+    def create_len_function(single_chain):
+        if single_chain:
+            return lambda x: llen <= len(x.cdr3aa) < hlen
+        else:
+            return lambda x: llen <= len(x.chainA.cdr3aa) < hlen and llen <= len(x.chainB.cdr3aa) < hlen
+
+    logging.info(f'Filtering: {llen} <= cdr3_len < {hlen}')
+    len_function = create_len_function(single_chain)
+
+    filtered_repertoire = repertoire.subsample_by_lambda(lambda x: not len_function(x))
+    if filtered_repertoire.total > 0:
+        for c in filtered_repertoire:
+            logging.warning(f'Filtered out clonotype with id {c.id} {c} due to length filter')
+
+    return repertoire.subsample_by_lambda(len_function)
+
+
+def load_mirpy_objects(segment_library, data_path, proto_path, locus=None,
+                       mapping_column=None, llen=None, hlen=None):
+    parser = AIRRParser(lib=segment_library,
+                        locus=locus) if locus is not None else DoubleChainAIRRParser(lib=segment_library,
+                                                                                     mapping_column=mapping_column)
     logging.info('Started loading clonotypes for analysis into MIR object.')
     analysis_repertoire = Repertoire.load(
-        parser=parser(lib=segment_library),
-        path=data_path
+        parser=parser,
+        path=data_path,
     )
+    analysis_repertoire = validate_cdr3_len(analysis_repertoire, llen, hlen, locus is not None)
     logging.info('Started loading prototypes into MIR object.')
     proto_repertoire = Repertoire.load(
-        parser=parser(lib=segment_library),
-        path=proto_path
+        parser=parser,
+        path=proto_path,
     )
     return analysis_repertoire, proto_repertoire
 
@@ -72,26 +102,43 @@ def main(args):
 
     chain = args.chain.split('_')
     single_chain = len(chain) == 1
+    locus = {'TRA': 'alpha', 'TRB': 'beta', 'TRA_TRB': None}[args.chain]
     segment_library = SegmentLibrary.load_default(genes=chain,
                                                   organisms=args.species)
 
-    analysis_repertoire, proto_repertoire = load_mirpy_objects(segment_library, input_path,
-                                                               proto_path, single_chain)
-    # todo prototypes might be single chain in this case???
+    analysis_repertoire, proto_repertoire = load_mirpy_objects(segment_library=segment_library,
+                                                               data_path=input_path,
+                                                               proto_path=proto_path,
+                                                               locus=locus,
+                                                               mapping_column=args.index_col,
+                                                               llen=args.lower_len_cdr3,
+                                                               hlen=args.higher_len_cdr3)
 
-    logging.info('Processed input clonotype and prototype data. Start calculating embedding.')
+    analysis_repertoire = analysis_repertoire.sample_n(n=args.n_clonotypes)
+    proto_repertoire = proto_repertoire.sample_n(n=args.n_prototypes)
+
+    logging.info('Processed input clonotype and prototype data. ')
+    logging.info(f'There are {analysis_repertoire.total} analysis clonotypes and {proto_repertoire.total} prototypes.')
+    logging.info('Initializing aligner.')
+    t0 = time.time()
+    aligner = ClonotypeAligner.from_library(lib=segment_library)
+    logging.info(f'Initialized aligner object in {time.time() - t0}. Initializing embedding object.')
 
     embedding_maker = PrototypeEmbedding(proto_repertoire,
-                                         aligner=ClonotypeAligner.from_library(
-                                             lib=segment_library)
+                                         aligner=aligner
                                          )
+    logging.info(f'Running the embedding calculation.')
+
     t0 = time.time()
     embeddings = embedding_maker.embed_repertoire(analysis_repertoire,
                                                   threads=args.nproc,
                                                   flatten_scores=True)
     column_names = []
     for i in range(proto_repertoire.total):
-        column_names += [f'{i}_a_v', f'{i}_a_j', f'{i}_a_cdr3', f'{i}_b_v', f'{i}_b_j', f'{i}_b_cdr3']
+        if 'TRA' in chain:
+            column_names += [f'{i}_a_v', f'{i}_a_j', f'{i}_a_cdr3']
+        if 'TRB' in chain:
+            column_names += [f'{i}_b_v', f'{i}_b_j', f'{i}_b_cdr3']
     embeddings = pd.DataFrame(embeddings, columns=column_names)
 
     logging.info(f'Finished {analysis_repertoire.total} clones in {time.time() - t0}')
